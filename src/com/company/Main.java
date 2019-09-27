333a650060669af7d4f80154d309d69eb005fa82
Dimitrijs Fedotovs <dima@fedoto.ws>
2019-09-27 14:11:18 +0300

    outer loop for repeating games

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,28 +9,38 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int myNum = rand.nextInt(100) + 1;
-        System.out.println(myNum);
+        String answer;
 
-        boolean userWon = false;
+        do {
+            int myNum = rand.nextInt(100) + 1;
+            System.out.println(myNum); // TODO remove this line once testing is finished
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Please, enter your guess:");
-            int userNum = scan.nextInt();
+            boolean userWon = false;
 
-            if (myNum > userNum) {
-                System.out.println("My number is greater than yours");
-            } else if (myNum < userNum) {
-                System.out.println("My number is less than yours");
-            } else {
-                System.out.println("Yeah! You won!");
-                userWon = true;
-                break;
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Please, enter your guess:");
+                int userNum = scan.nextInt();
+
+                if (myNum > userNum) {
+                    System.out.println("My number is greater than yours");
+                } else if (myNum < userNum) {
+                    System.out.println("My number is less than yours");
+                } else {
+                    System.out.println("Yeah! You won!");
+                    userWon = true;
+                    break;
+                }
             }
-        }
 
-        if (!userWon) {
-            System.out.println("Looser!");
-        }
+            if (!userWon) {
+                System.out.println("Looser!");
+            }
+
+            System.out.println("Do you want to play again? (y/n)");
+            answer = scan.next();
+
+        } while (answer.equalsIgnoreCase("y"));
+
+        System.out.println("Goodbye!");
     }
 }
